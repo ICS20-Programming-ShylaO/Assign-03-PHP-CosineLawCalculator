2218e096aad4853c1005d84a9c36df111bfebf19
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-13 15:38:59 +0000

    finishing touches on images and html

--- index.php
+++ index.php
@@ -25,17 +25,17 @@
 			echo "<h3>This program can calculate a side or angle of a triangle, given the other sides (and the corresponding side/angle to what you’re looking for). Keep in mind the capitalization and that we will be solving for side c or angle C.</h3>";
 		?>
     <center>
-    <img src="./images/cosinelaw_side.png" alt="Cosine Law for a Side" width="35%" height="35%">
-    <img src="./images/cosinelaw_angle.png" alt="Cosine Law for an Angle" width="35%" height="35%">
+    <img src="./images/cosine_law.png" alt="Cosine Law Formulas" width="35%" height="35%">
+    <img src="./images/triangle_visual.png" alt="Triangle with Side Labels" width="20%" height="20%">
     </center>
 		<!-- To solve side c -->
     <form action="./answers_side.php" method="post" target="results">
       <label for="lblSideA">Side a (cm):</label>
-      <input type="text" id="side-a" placeholder="Eg. 5" name="side-a"><br><br>
+      <input type="number" id="side-a" step="0.01" min="0" placeholder="Eg. 5" name="side-a"><br><br>
       <label for="lblSideB">Side b (cm):</label>
-      <input type="text" id="side-b" placeholder="Eg. 5" name="side-b"><br><br>
+      <input type="number" id="side-b" step="0.01" min="0" placeholder="Eg. 5" name="side-b"><br><br>
       <label for="lblAngleC">Angle C (deg):</label>
-      <input type="text" id="angle-c" placeholder="Eg. 75" name="angle-c"><br><br>
+      <input type="number" id="angle-c" step="0.01" min="0" placeholder="Eg. 75" name="angle-c"><br><br>
       <input type="submit" value="Calculate">
 		</form>
     <!-- iframe for the side*** results to show on the web page. -->
@@ -47,11 +47,11 @@
     <!-- To solve angle C -->
     <form action="./answers_angle.php" method="post" target="results-angle">
       <label for="lblSideA_2">Side a (cm):</label>
-      <input type="text" id="side-a-2" placeholder="Eg. 5" name="side-a-2"><br><br>
+      <input type="number" id="side-a-2" step="0.01" min="0" placeholder="Eg. 5" name="side-a-2"><br><br>
       <label for="lblSideB_2">Side b (cm):</label>
-      <input type="text" id="side-b-2" placeholder="Eg. 5" name="side-b-2"><br><br>
+      <input type="number" id="side-b-2" step="0.01" min="0" placeholder="Eg. 5" name="side-b-2"><br><br>
       <label for="lblSideC_2">Side c (cm):</label>
-      <input type="text" id="side-c-2" placeholder="Eg. 75" name="side-c-2"><br><br>
+      <input type="number" id="side-c-2" step="0.01" min="0" placeholder="Eg. 75" name="side-c-2"><br><br>
       <input type="submit" value="Calculate">
 		</form>
     <!-- iframe for the angle*** results to show on the web page. -->
